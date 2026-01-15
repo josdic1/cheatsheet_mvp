b4fac7eb168db9999e63b05a7f23f7aca02108bd
josdic1 <joshuadicker@gmail.com>
2026-01-15 10:09:20 -0500

    update again

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,9 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { routes } from './routes.jsx'
 import './index.css'
 
-const router = createBrowserRouter(routes)
+const router = createBrowserRouter(routes, {
+  basename: '/cheatsheet_mvp/'
+})
 
 const root = createRoot(document.getElementById('root'))
 root.render(<RouterProvider router={router} />)
